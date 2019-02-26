16ce25b1de0ec74b58fc7f02cc32649834b5e6ad
Henrique Lima <henrique.lima@grupopan.com>
2019-02-26 19:54:47 -0300

    Initial commit

---
+++ app/src/main/java/com/hpjokenpoha/a800041915/jokenpoha/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.hpjokenpoha.a800041915.jokenpoha
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
